98ed6c2e17f10921775fd028eebb4d713b1d42a1
Tri Quyet <nguyentriquyet010190@gmail.com>
2025-09-19 15:04:56 +0700

    fix domain

--- LexiMon.Repository/Domains/LessonProgress.cs
+++ LexiMon.Repository/Domains/LessonProgress.cs
@@ -9,11 +9,8 @@ public class LessonProgress : BaseAuditableEntity<Guid>
     public int CurrentValue { get; set; }
     public DateTimeOffset? StartDate { get; set; }
     public DateTimeOffset? EndDate { get; set; }
-    
-    public Guid LessonId { get; set; }
-    public Lesson Lesson { get; set; } = null!;
-    
+    public Guid? LessonId { get; set; }
+    public Lesson? Lesson { get; set; }
     public Guid? CustomLessonId { get; set; }
     public CustomLesson? CustomLesson { get; set; }
-    //public Order? Order { get; set; }
 }
