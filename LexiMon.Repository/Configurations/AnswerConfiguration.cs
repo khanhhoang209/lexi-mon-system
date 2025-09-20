b874418028d02692e15113d438cbd4ad2d1ace30
Tri Quyet <nguyentriquyet010190@gmail.com>
2025-09-20 15:32:40 +0700

    add domain configuration

---
+++ LexiMon.Repository/Configurations/AnswerConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using LexiMon.Repository.Domains;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LexiMon.Repository.Configurations;
+
+public class AnswerConfiguration : IEntityTypeConfiguration<Answer>
+{
+    public void Configure(EntityTypeBuilder<Answer> builder)
+    {
+        builder.ToTable("Answer");
+        builder.ConfigureAuditableProperties();
+        builder.HasKey(a => a.Id);
+
+        builder.Property(a => a.Content)
+            .IsRequired()
+            .HasMaxLength(1000);
+
+        builder.Property(a => a.IsCorrect)
+            .IsRequired();
+
+        builder.HasOne(a => a.Question)
+            .WithMany(q => q.Answers)
+            .HasForeignKey(a => a.QuestionId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ LexiMon.Repository/Configurations/LevelRangeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using LexiMon.Repository.Domains;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LexiMon.Repository.Configurations;
+
+public class LevelRangeConfiguration : IEntityTypeConfiguration<LevelRange>
+{
+    public void Configure(EntityTypeBuilder<LevelRange> builder)
+    {
+        builder.ToTable("LevelRange");
+        builder.ConfigureAuditableProperties();
+
+        builder.HasKey(lv => lv.Id);
+
+        builder.Property(lv => lv.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(lv => lv.FromExp).IsRequired();
+        builder.Property(lv => lv.ToExp).IsRequired();
+    }
+}
