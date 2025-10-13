677318527ab5cf22e38f6e406b5a995bfc7c4fe9
khanhhoang209 <lehoangkhanh209@gmail.com>
2025-10-13 21:29:17 +0700

    feat: add api dashboard

---
+++ LexiMon.Repository/Constants/Languages.cs
@@ -0,0 +1,9 @@
+﻿namespace LexiMon.Repository.Constants;
+
+public abstract class Languages
+{
+    public const string English = "Tiếng Anh";
+    public const string Japanese = "Tiếng Nhật";
+    public const string Korean = "Tiếng Hàn";
+    public const string Chinese = "Tiếng Trung";
+}
---
+++ LexiMon.Service/Models/Responses/CoinResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace LexiMon.Service.Models.Responses;
+
+public class CoinResponseDto
+{
+    public decimal ItemCoin { get; set; }
+    public decimal CourseCoin { get; set; }
+    public decimal TotalCoin => ItemCoin + CourseCoin;
+}
