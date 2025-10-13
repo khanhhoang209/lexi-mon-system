58c0b815206a1396b05f492ba8129e789a4855a4
khanhhoang209 <lehoangkhanh209@gmail.com>
2025-10-13 15:32:58 +0700

    feat: add api revenue

---
+++ LexiMon.Service/Interfaces/IDashboardService.cs
@@ -0,0 +1,11 @@
+﻿using LexiMon.Service.ApiResponse;
+using LexiMon.Service.Models.Requests;
+
+namespace LexiMon.Service.Interfaces;
+
+public interface IDashboardService
+{
+    Task<ServiceResponse> GetRevenueAsync(
+        RevenueRequestDto requestBody,
+        CancellationToken cancellationToken = default);
+}
